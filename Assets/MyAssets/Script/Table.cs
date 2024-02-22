4133a876eaefa4b0a1a245dde07344b7d5998477
Prosto_Dushnyi <papko.p@mail.ru>
2024-02-22 12:25:59 +0300

    add base logic for platform/tv

---
+++ Assets/MyAssets/Script/Table.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Rotate(0.5f, 0.1f, 0.2f);
+        transform.Rotate(0, 0.2f, 0);
+    }
+}
