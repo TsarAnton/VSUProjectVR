0b3fd4a4a9f7b628de33d7f7862c32823f966a19
Prosto_Dushnyi <papko.p@mail.ru>
2024-02-24 00:03:15 +0300

    add organ info depending on platform organ

--- Assets/MyAssets/Script/BorisOrgans.cs
+++ Assets/MyAssets/Script/BorisOrgans.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
 
 
 public class BorisOrgans : MonoBehaviour
 {
     [SerializeField] private float scaleMultiplier = 3f;
+    [SerializeField] public GameObject objectToSpawn; // Объект для спавна
+    public Transform spawnPoint;
     private Vector3 originalScale;
+    private GameObject spawnedObject;
     void Start()
     {
         originalScale = transform.localScale;
@@ -19,12 +26,25 @@ public class BorisOrgans : MonoBehaviour
     }
     public void ExitSocket()
     {
+        DestroyObject(spawnedObject);
         transform.localScale = originalScale;
         Debug.Log("Exit socket");
     }
     public void EnterSocket()
     {
-        transform.localScale = originalScale * scaleMultiplier; 
+        SpawnObject(objectToSpawn);
         Debug.Log("Entered socket");
+    }
+    private void SpawnObject(GameObject objectToSpawn)
+    {
+        spawnedObject = Instantiate(objectToSpawn, spawnPoint.position, objectToSpawn.transform.rotation);
+    }
+
+private void DestroyObject(GameObject spawnedObject)
+{
+        if (spawnedObject != null)
+        {
+            Destroy(spawnedObject);
+        }
     }
 }
