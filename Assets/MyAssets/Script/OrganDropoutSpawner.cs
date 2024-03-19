81bf8e740e136ec978be391efc5c8d9cd327df34
VonStille <papko.p@mail.ru>
2024-03-19 10:58:57 +0300

    dropout menu

--- Assets/MyAssets/Script/OrganDropoutSpawner.cs
+++ Assets/MyAssets/Script/OrganDropoutSpawner.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
+public class OrganToSpawn
+{
+    public GameObject objectToSpawn;
+    public Vector3 spawnOffset;
+    public Vector3 spawnRotation;
+}
 public class OrganDropoutSpawner : MonoBehaviour
 {
     public Transform spawnPoint;
     private GameObject spawnedObject;
-    public GameObject[] objectsToSpawn;
+    public List<OrganToSpawn> organsToSpawn;
+ 
     public void SpawnSelectedObject(int selectedIndex)
+    {
+        SpawnObject(selectedIndex, organsToSpawn[selectedIndex].spawnOffset, organsToSpawn[selectedIndex].spawnRotation);
+    }
+    private void SpawnObject(int selectedIndex, Vector3 spawnOffset, Vector3 spawnRotation)
     {
         if (spawnedObject != null)
         {
             Destroy(spawnedObject);
         }
-
+        Debug.Log(organsToSpawn.Count);
         // Проверяем, что выбран корректный индекс
-        if (selectedIndex >= 0 && selectedIndex < objectsToSpawn.Length)
+        if (selectedIndex >= 0 && selectedIndex < organsToSpawn.Count)
         {
             // Спавним выбранный объект
-            spawnedObject=Instantiate(objectsToSpawn[selectedIndex], transform.position, Quaternion.identity);
+            spawnedObject = Instantiate(organsToSpawn[selectedIndex].objectToSpawn, spawnPoint.position + spawnOffset, Quaternion.Euler(spawnRotation));
         }
+    }
+    public void RotateObject(float value)
+    {
+        Vector3 currentRotation = spawnedObject.transform.rotation.eulerAngles;
+        spawnedObject.transform.rotation = Quaternion.Euler(currentRotation.x, value * 360, currentRotation.z);
     }
 }
